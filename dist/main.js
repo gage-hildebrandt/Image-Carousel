dc01b2ce59c2a05cad9248ba26a01e0d79a7d65c
Gage Hildebrandt <vadium3854@gmail.com>
2024-07-14 08:42:39 -0400

    Created new functions for displaying slides
    Still need to add functionality

---
+++ dist/main.js
@@ -0,0 +1,18 @@
+
+const images = document.querySelectorAll("#slides img");
+let imageIndex = 0;
+let intervalID = null;
+
+function initializeSlider() {
+
+    images[imageIndex].classList.add("displayImage");
+}
+function slowImage (index) {
+
+}
+function prevSlide () {
+
+}
+function nextSlide () {
+
+}
